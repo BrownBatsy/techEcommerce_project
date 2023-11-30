3eabba7c00717e35869775569a4d9abd92c7bfa6
Pogen <76914800+IPogenI@users.noreply.github.com>
2023-12-01 04:08:24 +0600

    Working a bit on the FrontEnd
    A fun surprise awaits :D

---
+++ main.js
@@ -0,0 +1,28 @@
+document.getElementById('showProducts').addEventListener('click', function() {
+    document.getElementById('productList').style.display = 'flex';
+    document.getElementById('productDetails').style.display = 'none';
+});
+
+function viewProduct(productName, price) {
+    document.getElementById('productList').style.display = 'none';
+    document.getElementById('productDetails').style.display = 'block';
+    document.getElementById('productDetails').style.height = '80.2vh';
+
+    // Simulated product data (in a real-world scenario, this data would come from a server)
+    const products = {
+        'default': { name: `${productName}`, description: `Description of ${productName}. Lorem ipsum dolor sit amet, consectetur adipiscing elit.`, price: `${price}` },
+        // Add more products as needed
+
+    };
+    productName = 'default';
+    const product = products[productName];
+    console.log(products[productName]);
+    document.getElementById('productName').textContent = product.name;
+    document.getElementById('productDescription').textContent = product.description;
+    document.getElementById('productPrice').textContent = 'Price: ' + product.price;
+}
+
+function goBack() {
+    document.getElementById('productList').style.display = 'flex';
+    document.getElementById('productDetails').style.display = 'none';
+}
