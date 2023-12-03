f30f2660ffc2e3fd18b2b84f8b2f5b2b0972f9d5
Pogen <76914800+IPogenI@users.noreply.github.com>
2023-12-03 09:05:32 +0600

    fixed javascript cache issue

--- index.php
+++ index.php
@@ -1,4 +1,8 @@
 <?php
+//Making sure the server reloads the file each time the file is changed
+$etag = md5_file("main.js");
+header("ETag: $etag");
+
 //database connection establishment 
 //databse name: techdata // modify this part when needed 
 $server="localhost";
@@ -28,6 +32,9 @@ $gpus = mysqli_fetch_all($result, MYSQLI_ASSOC);
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv='cache-control' content='no-cache'>
+    <meta http-equiv='expires' content='0'>
+    <meta http-equiv='pragma' content='no-cache'>
     <link rel="stylesheet" type="text/css" href="style.css">
     <title>Tech Shop</title>
 </head>
@@ -90,7 +97,7 @@ $gpus = mysqli_fetch_all($result, MYSQLI_ASSOC);
 
     </section>
 
-    <section id="productDetails" style="display: none;>
+    <section id="productDetails" style="display: none;">
         <h2 id="productName"></h2>
         <p id="productDescription"></p>
         <p id="productPrice"></p>
@@ -162,35 +169,6 @@ $gpus = mysqli_fetch_all($result, MYSQLI_ASSOC);
     </footer>
 
 <!--Adding Javascript-->
-<script type="text/javascript">
-    document.getElementById('showProducts').addEventListener('click', function() {
-        document.getElementById('productList').style.display = 'flex';
-        document.getElementById('productDetails').style.display = 'none';
-    });
-
-    function viewProduct(productName, price) {
-        document.getElementById('productList').style.display = 'none';
-        document.getElementById('productDetails').style.display = 'block';
-        document.getElementById('productDetails').style.height = 'fit-content';
-
-        // Simulated product data (in a real-world scenario, this data would come from a server)
-        const products = {
-            'default': { name: `${productName}`, description: `Description of ${productName}. Lorem ipsum dolor sit amet, consectetur adipiscing elit.`, price: `${price}` },
-            // Add more products as needed
-
-        };
-        productName = 'default';
-        const product = products[productName];
-        console.log(products[productName]);
-        document.getElementById('productName').textContent = product.name;
-        document.getElementById('productDescription').textContent = product.description;
-        document.getElementById('productPrice').textContent = 'Price: ' + product.price;
-    }
-
-    function goBack() {
-        document.getElementById('productList').style.display = 'flex';
-        document.getElementById('productDetails').style.display = 'none';
-    }
-</script>
+<script type="text/javascript" src="main.js"></script>
 </body>
 </html>
